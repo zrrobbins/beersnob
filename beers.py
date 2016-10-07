7a66f7674d32cbdd0aa576f8a281b985eae74530
Zachary Robbins <zrrobbins@wpi.edu>
2016-10-07 19:33:48 -0400

    Added api test file.

---
+++ beers.py
@@ -0,0 +1,14 @@
+import requests
+import json
+from pprint import pprint
+import os
+
+
+BEER_API_KEY = os.environ['BEER_API_KEY']
+
+response = requests.get(
+	"http://api.brewerydb.com/v2/beers/",
+    params={'key':API_KEY}
+)
+pprint(response.status_code)
+pprint(json.loads(response.content.decode()))
