43b0b56941a344f8d73c83dce3ac64537a6ba46f
Kyle McCormick <kylemccor@gmail.com>
2016-10-07 20:02:58 -0400

    Change API_KEY -> BEER_API_KEY

--- beers.py
+++ beers.py
@@ -8,7 +8,7 @@ BEER_API_KEY = os.environ['BEER_API_KEY']
 
 response = requests.get(
 	"http://api.brewerydb.com/v2/beers/",
-    params={'key':API_KEY}
+    params={'key':BEER_API_KEY}
 )
 pprint(response.status_code)
 pprint(json.loads(response.content.decode()))
