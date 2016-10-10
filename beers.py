72bb0c6f9b69a66ddc30843ec6285122a85f4a5c
Zachary Robbins <zrrobbins@wpi.edu>
2016-10-10 00:37:31 -0400

    Added beer data download script.

--- beers.py
+++ beers.py
@@ -1,3 +1,10 @@
+"""
+download_beer_data.py
+
+This script downloads the entire beer database from BreweryDB.
+(WARNING: THIS TAKES A LONG TIME!)
+"""
+
 import requests
 import json
 from pprint import pprint
@@ -5,14 +12,28 @@ import os
 
 
 BEER_API_KEY = os.environ['BEER_API_KEY']
+BEER_DATA_DIR = "beer_data/"
 
-response = requests.get(
+if not os.path.exists(BEER_DATA_DIR):
+    os.makedirs(BEER_DATA_DIR)
+
+numberOfPages = requests.get(
 	"http://api.brewerydb.com/v2/beers/",
-    params={'key':BEER_API_KEY, 'p':1}
+    params={'key':BEER_API_KEY, 'p':1000000}
 )
-pprint(response.status_code)
-pprint(json.loads(response.content.decode()))
 
-with open('beers_page1.txt', 'w') as outfile:
-     json.dumps(json.loads(response.content.decode()))
+numberOfPages = json.loads(numberOfPages.content.decode())['numberOfPages']
 
+print("Downloading {} pages of beer...".format(numberOfPages))
+
+for page in range(1, numberOfPages+1):
+	response = requests.get(
+		"http://api.brewerydb.com/v2/beers/",
+	    params={'key':BEER_API_KEY, 'p':page}
+	)
+	outfile_name = BEER_DATA_DIR + "beers_page{}.json".format(page)
+
+	with open(outfile_name, 'w') as outfile:
+		outfile.write(response.content.decode())
+
+print("Finished!")
