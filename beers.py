b9d08913fa2fc088f5dc6e825bc9c86b6fe1ab48
Zachary Robbins <zrrobbins@wpi.edu>
2016-10-09 20:29:44 -0400

    Decided on scikit-learn as ML library, included test file.

--- beers.py
+++ beers.py
@@ -8,7 +8,11 @@ BEER_API_KEY = os.environ['BEER_API_KEY']
 
 response = requests.get(
 	"http://api.brewerydb.com/v2/beers/",
-    params={'key':API_KEY}
+    params={'key':BEER_API_KEY, 'p':1}
 )
 pprint(response.status_code)
 pprint(json.loads(response.content.decode()))
+
+with open('beers_page1.txt', 'w') as outfile:
+     json.dumps(json.loads(response.content.decode()))
+
