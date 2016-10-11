6ccb2ce11fca8c01d1829062801eeb8449ba6ac5
Zachary Robbins <zrrobbins@wpi.edu>
2016-10-11 18:30:20 -0400

    Added more classifiers to main run file. Renamed mlp.py.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 """
-mlp.py
+main.py
 
-Multi-layer perceptron (deep neural network) test file.
+Classifier testing file.
 """
 import os
 import json
 
-from sklearn import neural_network
 from sklearn.feature_extraction import DictVectorizer
 from matplotlib.colors import ListedColormap
 from sklearn.model_selection import train_test_split
@@ -25,21 +24,19 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from data_transformer import trim_data
 from data_transformer import FLATTENED_BEER_DATA_DIRECTORY
 
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM",
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes", "QDA"]
+         "Naive Bayes"]
 
 classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
     SVC(gamma=2, C=1),
-    GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     MLPClassifier(hidden_layer_sizes=(100,), max_iter=1000),
     AdaBoostClassifier(),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis()]
+    GaussianNB()]
 
 TRAINING_DATA = .5 # Percent training data
 
@@ -48,7 +45,7 @@ with open(os.path.join(FLATTENED_BEER_DATA_DIRECTORY, 'flattened_beer_data.json'
     json_data = json.loads(json_data)
     # Consider only passing slices of the data in here, or the classifier could take a VERY long time to run!
     # Some datasets take longer to classify than others (e.g. trim_level=1 takes longer than trim_level=0)
-    trimmed_beers = trim_data({'data': json_data['data'], 'labels': json_data['labels']}, 3)
+    trimmed_beers = trim_data({'data': json_data['data'][:10000], 'labels': json_data['labels'][:10000]}, 0)
 
 vectorizer = DictVectorizer(sparse=False) # Used to transform beer dict into numpy arrays so classifiers can use
 
@@ -58,19 +55,14 @@ labels = trimmed_beers['labels']
 n_samples = len(trimmed_beers['data'])
 print("Trimmed data len: {}".format(n_samples))
 
-X_train = beers[:int(TRAINING_DATA * n_samples)]
-y_train = labels[:int(TRAINING_DATA * n_samples)]
-X_test = beers[int(TRAINING_DATA * n_samples):]
-y_test = labels[int(TRAINING_DATA * n_samples):]
-
-# TODO: Try other classifiers (i.e. Naive Bayes, KNN, SVM, Decision Trees, etc.)
+X_train, X_test, y_train, y_test = train_test_split(beers, labels, test_size=.25, random_state=42)
 
 print("Calculating...\n")
-# hidden_layer_sizes=(100, 100, 100) ==> Neural network with 3 hidden layers of 100 units each (5 total layers)
-# max_iter is the number of iterations we will stop the simulations if it doesn't converge by that point
-mlp_classifier = neural_network.MLPClassifier(hidden_layer_sizes=(100, ), max_iter=1000)
+for name, clf in zip(names, classifiers):
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    print('{} score: {}'.format(name, score))
 
-print('MLP score: {}'.format(mlp_classifier.fit(X_train, y_train).score(X_test, y_test)))
-print("Predicted probabilities for beer types: \n{}".format(mlp_classifier.predict_proba(beers[-1].reshape(1,-1))))
-print("Predicted outcome for last beer: {}".format(mlp_classifier.predict(beers[-1].reshape(1,-1))))
-print("Actual outcome for last beer: {}".format(labels[-1]))
+# print("Predicted probabilities for beer types: \n{}".format(mlp_classifier.predict_proba(beers[-1].reshape(1,-1))))
+# print("Predicted outcome for last beer: {}".format(mlp_classifier.predict(beers[-1].reshape(1,-1))))
+# print("Actual outcome for last beer: {}".format(labels[-1]))
